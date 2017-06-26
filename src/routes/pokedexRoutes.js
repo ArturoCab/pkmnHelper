463d0bf6eb6da8400f4525b0e4f2fe1dfc8f4ff1
Arturo Cabrera <cabrera_a90@hotmail.com>
2017-06-26 00:14:56 -0500

    pokedex is visible

--- src/routes/pokedexRoutes.js
+++ src/routes/pokedexRoutes.js
@@ -1,41 +1,20 @@
-var express=require('express');
-var mysql=require('mysql');
+var express =  require('express');
+var pkdxDB = require('../model/pokedexDB.js');
 
 var pokedexRouter=express.Router();
-var ppl=0;
-var pool=mysql.createPool({
-  connectionLimit:100,
-  host:'localhost',
-  user:'root',
-  password:'root',
-  database:'mydb',
-  debug:false
-});
 
-function handle_database(req, res){
-  pool.getConnection(function(err,connection){
-    if(err){
-      res.json({"code":100,"status":"Error in connection database"})
-      return;
-    }
-    console.log('connected as id '+ppl++);
-    connection.query("select * from pkm_pokedex",function(err, rows){
-      connection.release();
-      if(!err){
-        res.json(rows);
-      }
-    });
-    connection.on('error',function(err){
-      res.json({"code":100,"status":"Error in connection database"})
-      return;
-    });
-  });
-}
-pokedexRouter.route('/')
+pokedexRouter.route("/")
   .get(function(req,res){
-    console.log('pokedex');
-    handle_database(req,res)
-    //res.status(200).send('Hello and welcome to the pokedex\nThis is under construction');
+    pkdxDB.getMessages().then(function(messages){
+      res.render('pokedex',{
+        title:'RotomDex',
+        'messages':messages
+        //'menuList':menuList
+      });
+      console.log('pokedex');
+      console.log(messages);
+    });
+    //handle_database(req,res);
   });
 
-module.exports=pokedexRouter;
+module.exports = pokedexRouter;
